81a6e24d96b7df395e1a7e00d5df9919c38df750
acpatison@gmail.com <acpatison@gmail.com>
2021-07-12 12:43:16 -0400

    added files, bio information, and structure

---
+++ src/resumeData.js
@@ -0,0 +1,116 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Rahul Bhatia",
+    "role": "Frontend Developer and Data Scientist",
+    "linkedinId":"Your LinkedIn Id",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/rahul-bhatia-67ba08121/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/acpatison",
+          "className":"fa fa-github"
+        },
+        // {
+        //   "name":"skype",
+        //   "url":"http://twitter.com/rbhatia46",
+        //   "className":"fa fa-twitter"
+        // }
+      ],
+    "aboutme":"My name is Andrew Patison and welcome to my ongoing work portfolio. I currently work as a Software Consultant in Atlanta for the firm Manhattan Associates. As a consultant, I work with client to implement Warehouse Management software with distribution centers all over North America. In addition, to work some of my hobbies include: furthering my professional development in as many ways as possible, running outside or working out, trying new foods, traveling, sports and hanging out with friends. Today, I am a new coder who is learning how to work with HTML, JavaScript, CSS and other coding languages so that I may be able to do better in my current consultant role with Manhattan Associates. I hope you look through some of my work below and can see my progress as we move along. Let me know if you like what you see!",
+    "address":"Atlanta, Georgia, USA",
+    // "website":"https://rbhatia46.github.io",
+    "education":[
+      {
+        "UniversityName":"University of South Carolina - Darla Moore School of Business",
+        "specialization":"Finance and Operations & Supply Chain",
+        "MonthOfPassing":"December",
+        "YearOfPassing":"2020",
+        "Achievements":"Graduation with Leadership Distinction and Honors"
+      },
+      {
+        "UniversityName":"Georgia Tech Professional Education",
+        "specialization":"Full-Stack Web Development Certification",
+        "MonthOfPassing":"July",
+        "YearOfPassing":"2021",
+        // "Achievements":"Some Achievements"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Manhattan Associates",
+        "specialization":"Software Consultant",
+        // "MonthOfLeaving":"Jan",
+        // "YearOfLeaving":"2018",
+        // "Achievements":"Some Achievements"
+      },
+    //   {
+    //     "CompanyName":"Some Company",
+    //     "specialization":"Some specialization",
+    //     "MonthOfLeaving":"Jan",
+    //     "YearOfLeaving":"2018",
+    //     "Achievements":"Some Achievements"
+    //   }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname":"Nodejs"
+      },
+      {
+        "skillname": "Lean-Six-Sigma Green Belt"
+      },
+      {
+        "skillname": "Software Implementation"
+      }
+    ],
+    // Take portfolio projects from your original portfolio:
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    // "testimonials":[
+    //   {
+    //     "description":"This is a sample testimonial",
+    //     "name":"Some technical guy"
+    //   },
+    //   {
+    //     "description":"This is a sample testimonial",
+    //     "name":"Some technical guy"
+    //   }
+    // ]
+  }
+  
+  export default resumeData;
