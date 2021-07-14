27c7835eb31a46d25df7a634012488b3174eaac2
acpatison@gmail.com <acpatison@gmail.com>
2021-07-14 14:16:45 -0400

    added attributes to Project working tiles for portfolio

---
+++ src/components/portfolio.js
@@ -0,0 +1,26 @@
+import React from 'react'; 
+import ProjectTiles from './projects'
+import portfolio from '../porfolio.json'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function Wrapper(props) {
+    return <div className="wrapper">{props.children}</div>;
+}
+
+function Portfolio () {  
+    return (
+        <section className="container">
+          <div className="project">
+            <h2 className="top-title">My Working Portfolio</h2>
+            <hr></hr>
+          </div>
+          <Wrapper id="card-data">
+            {portfolio.map((project) => (
+              <ProjectTiles key={project.id} image={project.image} name={project.name} github={project.github} deploy={project.deploy} topics={project.topics}/>
+            ))}
+          </Wrapper>
+        </section>
+    );
+  }
+
+export default Portfolio;
